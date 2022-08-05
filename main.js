121189d855b5e9aa103ec8df37d86ad58ec28fe1
Latha <lathasreereddy27@gmail.com>
2022-08-05 09:05:24 +0530

    checking from atom

---
+++ main.js
@@ -0,0 +1,18 @@
+"use strict"
+// var age_list = [22, 55, 14, 31, 20, 11];
+//
+// filterFunc = (age, calculate) => {
+//     var result = [];
+//     var index = 0;
+//     for(var i = 0 ; i < age.length ; i++) {
+//         if(calculate(age[i]))
+//             result[index++] = age[i];
+//     }
+//     return result;
+// };
+//
+// checkAdult = (age) => {
+//         return age >= 18;
+// };
+//
+// console.log(filterFunc(age_list, checkAdult));
